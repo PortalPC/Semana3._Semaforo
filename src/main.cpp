38ce2155fc6df7976c5e3b6a732ba2782795f015
PortalPC <ian.portal@usil.pe>
2025-05-05 11:37:47 -0500

    Actualización_Portal_1

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,14 @@
 #include <avr/io.h>
 #include<util/delay.h>
 
-int main (void){
-	DDRD|= 0xE0;
-	while (1){
-		PORTD&= 0x1F;
-		PORTD|= 0x80;
-		_delay_ms(30000);
-		PORTD&= 0x1F;
-		PORTD|= 0x40;
-		_delay_ms(3000);
-		PORTD&= 0x1F;
-		PORTD|= 0x20;
-		_delay_ms(15000);
-	}
+int main(void){
+  DDRD |= 0xE0;
+  while(1){
+    PORTD &= 0x1F;
+    PORTD |= 0x80;
+    _delay_ms(3000);
+    PORTD &= 0x1F;
+    PORTD |= 0x40;
+    _delay_ms(3000);
+  }
 }
