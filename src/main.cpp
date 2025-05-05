6286f63a3fe9d7ded5b2d832fa8fe29d15cae88a
MezaRE <rodrigo.meza@usil.pe>
2025-05-05 11:43:30 -0500

    Actualización_Meza_2

--- src/main.cpp
+++ src/main.cpp
@@ -10,5 +10,8 @@ int main(void){
     PORTD &= 0x1F;
     PORTD |= 0x40;
     _delay_ms(3000);
+	PORTD &= 0x1F;
+	PORTD |= 0x20;
+	_delay_ms(150000); // Ya esta configurado el delay, se incluyó arriba en la primera linea del codigo "#"
   }
 }
